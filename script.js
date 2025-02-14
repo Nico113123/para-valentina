bb2cca5c8abe1ed4c1f883cf2de234c2a78586dd
Nico113123 <nicolasabril391@gmail.com>
2025-02-14 17:24:11 -0500

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const card = document.getElementById("valentineCard");
-
-    card.addEventListener("click", function () {
-        card.classList.toggle("open"); // Alternar la clase 'open' para girar la carta
-    });
-});
